c60e69cb729058c3f70165e8efb30c010fe08d47
Dan Barela <daniel.barela@gmail.com>
2022-08-11 08:39:58 -0600

    rename the app

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/taketwo'),
+      dir: require('path').join(__dirname, './coverage/geopackage-viewer-js'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'taketwo'`, () => {
+  it(`should have as title 'geopackage-viewer-js'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('taketwo');
+    expect(app.title).toEqual('GeoPackage Viewer');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('taketwo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('GeoPackage Viewer app is running!');
   });
 });
